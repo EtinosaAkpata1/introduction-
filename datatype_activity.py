31d35b4ffe1588d4ec477edec233e52023c206ed
EtinosaAkpata1 <etinakpata@gmail.com>
2026-03-08 18:27:59 -0500

    Add files via upload

---
+++ datatype_activity.py
@@ -0,0 +1,12 @@
+num1=45
+print(num1)
+print(type(num1))#integer
+num2=7.90
+print(num2)
+print(type(num2))#float
+result=False
+print(result)
+print(type(result))
+name="Etin "
+print(name)
+print(type(name))
