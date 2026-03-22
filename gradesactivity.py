b1e5b233294f95137d1a4d1e1243064a3b2a126a
EtinosaAkpata1 <etinakpata@gmail.com>
2026-03-22 18:05:59 -0500

    Add files via upload

---
+++ gradesactivity.py
@@ -0,0 +1,15 @@
+# take marks as input from user
+print("Enter Marks Obtained in 4 Subjects: ")
+math = int(("maths :"))
+english = int(("english 90 :"))
+science = int(("science 90:"))
+spanish = int(("spanish 90:"))
+
+# Let's calculate the percentage of marks
+sum = math+english+science+hindi
+print("sum of math,english,science and hindi = ",sum)
+
+perc = (sum/400)*100
+
+print(end="Percentage Mark = ")
+print(perc)
